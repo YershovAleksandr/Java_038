ffbfa30ffa515bcbf6238668ed623a3209c31a63
hp <namstudionsk@gmail.com>
2018-12-05 23:46:29 +0700

    Add result class

---
+++ src/main/java/com/nam/Result.java
@@ -0,0 +1,39 @@
+package com.nam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Result {
+    private final List<String> removedPages = new ArrayList<>();
+    private final List<String> addedPages = new ArrayList<>();
+    private final List<String> changedPages = new ArrayList<>();
+
+    public Result(){
+    }
+
+    public List<String> getRemovedPages() {
+        return removedPages;
+    }
+
+    public List<String> getAddedPages(){
+        return addedPages;
+    }
+
+    public List<String> getChangedPages(){
+        return changedPages;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Здравствуйте, дорогая и.о. секретаря\n" +
+                "За последние сутки во вверенных Вам сайтах произошли следующие изменения:\n" +
+                "\n" +
+                "\t1. Исчезли следующие страницы: %s\n" +
+                "\t2. Появились следующие новые страницы: %s\n" +
+                "\t3. Изменились следующие страницы: %s\n" +
+                "\n" +
+                "С уважением,\n" +
+                "автоматизированная система\n" +
+                "мониторинга.", removedPages.toString(), addedPages.toString(), changedPages.toString());
+    }
+}
